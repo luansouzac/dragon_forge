b9cffe7a77038d1c77bc34fff02477662736ca62
Luan Souza <luansouzac14@gmail.com>
2025-10-14 22:09:53 -0300

    feat: nome do repositorio para o github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dragon-forge/', 
+  base: '/dragon_forge/',
   plugins: [
     vue(),
     vueDevTools(),
