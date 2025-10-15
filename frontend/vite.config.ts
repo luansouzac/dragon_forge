dd5d84d61b7f9fc1d3885208a7fd047aff66dcc6
Luan Souza <luansouzac14@gmail.com>
2025-10-14 22:00:38 -0300

    Prepare for deployment to GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dragon-forge/', 
   plugins: [
     vue(),
     vueDevTools(),
