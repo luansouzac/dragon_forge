09243d10b9baed44241a97e082df7ea13bda5ac3
Luan Souza <luansouzac14@gmail.com>
2025-10-14 21:30:39 -0300

    feat: Adiciona bootstrap 5

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+
 
 const app = createApp(App)
 
