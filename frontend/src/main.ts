19d59a9de58f4f2b5c1a2b06e5d6344bd4472343
Luan Souza <luansouzac14@gmail.com>
2025-10-14 21:34:57 -0300

    feat: Configura variaveis do bootstrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
-
+import './assets/scss/main.scss'
 
 const app = createApp(App)
 
